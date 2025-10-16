3a6631a076a0f1c3de07f298530c3e560ba88c3e
TuteMthCD <matias.civadda2342001@gmail.com>
2025-10-16 04:05:17 -0300

    added: ws2812 neopixel driver skeleton

--- src/ws2812.rs
+++ src/ws2812.rs
@@ -1,14 +1,10 @@
-use std::time::Duration;
-
 use anyhow::Result;
-use esp_idf_hal::delay::FreeRtos;
-use log::info;
-
 use esp_idf_hal::{
     gpio::OutputPin,
     peripheral::Peripheral,
-    rmt::{self, FixedLengthSignal, RmtChannel},
+    rmt::{self, RmtChannel},
 };
+use log::info;
 
 pub fn ws2812_task<C, CH, P, PIN>(channel: CH, pin: PIN) -> Result<()>
 where
@@ -20,45 +16,166 @@ where
     info!("Init ws2812_task");
     let conf = rmt::TxRmtConfig::new().clock_divider(1);
 
-    let mut tx = rmt::TxRmtDriver::new(channel, pin, &conf)?;
+    let rmt = rmt::TxRmtDriver::new(channel, pin, &conf)?;
 
-    let ticks_hz = tx.counter_clock()?;
-    info!("RMT counter clock: {} Hz", ticks_hz.0);
+    let mut ledstrip = neopixel::Ws2812::new(rmt)?;
 
-    let (t0h, t0l, t1h, t1l) = (
-        rmt::Pulse::new_with_duration(ticks_hz, rmt::PinState::High, &Duration::from_nanos(350))?,
-        rmt::Pulse::new_with_duration(ticks_hz, rmt::PinState::Low, &Duration::from_nanos(800))?,
-        rmt::Pulse::new_with_duration(ticks_hz, rmt::PinState::High, &Duration::from_nanos(700))?,
-        rmt::Pulse::new_with_duration(ticks_hz, rmt::PinState::Low, &Duration::from_nanos(600))?,
-    );
+    let pixels = vec![
+        neopixel::Rgb::new(255, 255, 255),
+        neopixel::Rgb::new(255, 0, 0),
+        neopixel::Rgb::new(0, 255, 0),
+    ];
 
-    let mut color = 0x00_00_00;
+    ledstrip.transmit(&pixels)?;
 
-    loop {
-        color += 0xFF;
-        if let Ok(signal) = encode_color(color, t0h, t0l, t1h, t1l) {
-            tx.start_blocking(&signal)?;
-            info!("frame sent {:x}", color);
-        }
-        FreeRtos::delay_ms(1000);
-    }
+    Ok(())
 }
 
-fn encode_color(
-    code: u32,
-    t0h: rmt::Pulse,
-    t0l: rmt::Pulse,
-    t1h: rmt::Pulse,
-    t1l: rmt::Pulse,
-) -> Result<FixedLengthSignal<24>> {
-    let mut signal = FixedLengthSignal::<24>::new();
+mod neopixel {
+    use std::time::Duration;
 
-    for i in (0..24).rev() {
-        let p = 1_u32 << i;
-        let bit_set = p & code != 0;
-        let (high_pulse, low_pulse) = if bit_set { (t1h, t1l) } else { (t0h, t0l) };
-        signal.set(23 - i as usize, &(high_pulse, low_pulse))?;
+    use anyhow::{bail, Result};
+    use esp_idf_hal::rmt::{self, TxRmtDriver, VariableLengthSignal};
+
+    struct Timings {
+        t0h: rmt::Pulse,
+        t0l: rmt::Pulse,
+        t1h: rmt::Pulse,
+        t1l: rmt::Pulse,
+        rst: rmt::Pulse,
     }
 
-    Ok(signal)
+    pub struct Ws2812<'d> {
+        pixels: Vec<Rgb>,
+        tx: TxRmtDriver<'d>,
+        timings: Timings,
+    }
+
+    impl<'d> Ws2812<'d> {
+        pub fn new(tx: TxRmtDriver<'d>) -> Result<Self> {
+            let pixels = Vec::new();
+
+            let ticks_hz = tx.counter_clock()?;
+
+            let timings = Timings {
+                t0h: rmt::Pulse::new_with_duration(
+                    ticks_hz,
+                    rmt::PinState::High,
+                    &Duration::from_nanos(350),
+                )?,
+                t0l: rmt::Pulse::new_with_duration(
+                    ticks_hz,
+                    rmt::PinState::Low,
+                    &Duration::from_nanos(800),
+                )?,
+                t1h: rmt::Pulse::new_with_duration(
+                    ticks_hz,
+                    rmt::PinState::High,
+                    &Duration::from_nanos(700),
+                )?,
+                t1l: rmt::Pulse::new_with_duration(
+                    ticks_hz,
+                    rmt::PinState::Low,
+                    &Duration::from_nanos(600),
+                )?,
+                rst: rmt::Pulse::new_with_duration(
+                    ticks_hz,
+                    rmt::PinState::Low,
+                    &Duration::from_nanos(50),
+                )?,
+            };
+
+            Ok(Self {
+                pixels,
+                tx,
+                timings,
+            })
+        }
+
+        fn encode_signal(
+            timings: &Timings,
+            pixels: &[Rgb],
+        ) -> Result<VariableLengthSignal, anyhow::Error> {
+            let mut signal = VariableLengthSignal::with_capacity(pixels.len() * 2 * 24 + 1);
+
+            for pixel in pixels {
+                let color = u32::from(pixel);
+                for bit_idx in (0..24).rev() {
+                    let bit = (color >> bit_idx) & 1 != 0;
+                    let (high_pulse, low_pulse) = if bit {
+                        (timings.t1h, timings.t1l)
+                    } else {
+                        (timings.t0h, timings.t0l)
+                    };
+                    signal.push(&[high_pulse, low_pulse])?;
+                }
+            }
+
+            signal.push(&[timings.rst])?;
+
+            Ok(signal)
+        }
+
+        pub fn transmit(&mut self, pixels: &[Rgb]) -> Result<()> {
+            self.pixels.clear();
+            self.pixels.extend_from_slice(pixels);
+            let signal = Self::encode_signal(&self.timings, &self.pixels)?;
+            let res = self.tx.start_blocking(&signal)?;
+            Ok(res)
+        }
+    }
+
+    #[derive(Clone, Copy)]
+    pub struct Rgb {
+        r: u8,
+        g: u8,
+        b: u8,
+    }
+
+    impl Rgb {
+        pub fn new(r: u8, g: u8, b: u8) -> Self {
+            Self { r, g, b }
+        }
+        /// Converts hue, saturation, value to RGB
+        pub fn from_hsv(h: u32, s: u32, v: u32) -> Result<Self, anyhow::Error> {
+            if h > 360 || s > 100 || v > 100 {
+                bail!("The given HSV values are not in valid range");
+            }
+            let s = s as f64 / 100.0;
+            let v = v as f64 / 100.0;
+            let c = s * v;
+            let x = c * (1.0 - (((h as f64 / 60.0) % 2.0) - 1.0).abs());
+            let m = v - c;
+            let (r, g, b) = match h {
+                0..=59 => (c, x, 0.0),
+                60..=119 => (x, c, 0.0),
+                120..=179 => (0.0, c, x),
+                180..=239 => (0.0, x, c),
+                240..=299 => (x, 0.0, c),
+                _ => (c, 0.0, x),
+            };
+            Ok(Self {
+                r: ((r + m) * 255.0) as u8,
+                g: ((g + m) * 255.0) as u8,
+                b: ((b + m) * 255.0) as u8,
+            })
+        }
+    }
+
+    impl From<&Rgb> for u32 {
+        /// Convert RGB to u32 color value
+        ///
+        /// e.g. rgb: (1,2,4)
+        /// G        R        B
+        /// 7      0 7      0 7      0
+        /// 00000010 00000001 00000100
+        fn from(rgb: &Rgb) -> Self {
+            ((rgb.g as u32) << 16) | ((rgb.r as u32) << 8) | rgb.b as u32
+        }
+    }
+    impl From<Rgb> for u32 {
+        fn from(rgb: Rgb) -> Self {
+            (&rgb).into()
+        }
+    }
 }
